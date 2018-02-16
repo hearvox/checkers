384d2b6b4a1c55c32d4ffd7ae8d3cf83ada841a6
Barrett Golding <bgMBP@BG-MBP.local>
2018-02-16 10:54:18 -0700

    Pass arrays from PHP to JS

--- checkers.php
+++ checkers.php
@@ -24,37 +24,7 @@ if ( defined( 'checkers_VERSION' ) ) {
     return;
 }
 
-/**
- * Adds "Settings" link on Plugins screen (next to "Activate").
-
- * @since  0.1.0
- *
- * @param   string  $links HTML links for Plugins screen.
- * @return  string  $links HTML links for Plugins screen.
- */
-function checkers_plugin_settings_link( $links ) {
-  $settings_link = '<a href="' . esc_url( admin_url( 'options-general.php?page=checkers' ) ) . '">' . __( 'Settings', 'checkers' ) . '</a>';
-  array_unshift( $links, $settings_link );
-  return $links;
-}
-add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'checkers_plugin_settings_link' );
-
-/* ------------------------------------------------------------------------ *
- * Constants: plugin path, URI, dir, filename, and version.
- *
- * CHECKERS_BASENAME     checkers/checkers.php
- * CHECKERS_DIR          /path/to/wp-content/plugins/checkers/
- * CHECKERS_DIR_BASENAME checkers/
- * CHECKERS_URI https://example.com/wp-content/plugins/checkers/
- * ------------------------------------------------------------------------ */
 define( 'CHECKERS_VERSION', '0.1.0' );
-define( 'CHECKERS_BASENAME', plugin_basename( __FILE__ ) );
-define( 'CHECKERS_DIR', trailingslashit( plugin_dir_path( __FILE__ ) ) );
-define( 'CHECKERS_URI', trailingslashit( plugin_dir_url( __FILE__ ) ) );
-define(
-    'CHECKERS_DIR_BASENAME',
-    trailingslashit( dirname( plugin_basename( __FILE__ ) ) )
-);
 
 /**
  * Load the plugin text domain for translation.
@@ -71,6 +41,21 @@ function checkers_load_textdomain() {
         );
 }
 add_action( 'plugins_loaded', 'checkers_load_textdomain' );
+
+/**
+ * Adds "Settings" link on Plugins screen (next to "Activate").
+
+ * @since  0.1.0
+ *
+ * @param   string  $links HTML links for Plugins screen.
+ * @return  string  $links HTML links for Plugins screen.
+ */
+function checkers_plugin_settings_link( $links ) {
+  $settings_link = '<a href="' . esc_url( admin_url( 'options-general.php?page=checkers' ) ) . '">' . __( 'Settings', 'checkers' ) . '</a>';
+  array_unshift( $links, $settings_link );
+  return $links;
+}
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'checkers_plugin_settings_link' );
 
 /**
  * Redirect to Settings screen upon plugin activation.
@@ -90,6 +75,57 @@ function checkers_activation_redirect( $plugin ) {
     }
 }
 add_action( 'activated_plugin', 'checkers_activation_redirect' );
+
+
+/* ------------------------------------------------------------------------ *
+ * Site checking services
+ * ------------------------------------------------------------------------ */
+/*
+ * Webpage-checking services that process results with URL in query string.
+ *
+ * array({Service-name}, {Service-URL-prefix}, {encode?}, {dashicon})
+ */
+$checkers_pages = array(
+      array('Google: Pagespeed Insights', 'https://developers.google.com/speed/pagespeed/insights/?url=', 1, 'performance'),
+      array('Google: Mobile-Friendly Test', 'https://search.google.com/test/mobile-friendly?url=', 1, 'performance'),
+      array('W3C: Markup Validation', 'https://validator.w3.org/checklink?hide_type=all&depth=&check=Check&uri=', 1, 'performance'),
+      array('Twitter: Search (post URL)', 'https://twitter.com/search?src=typd&q=', 1, 'share'),
+      array('Facebook: Link Preview', 'https://developers.facebook.com/tools/debug/sharing/?q=', 1, 'share'),
+      array('Facebook: Shares (data)', 'https://graph.facebook.com/?id=', 0, 'share'),
+      array('LinkedIn: Shares (data)', 'https://www.linkedin.com/countserv/count/share?url=', 0, 'share'),
+      array('Moz: Open Site Explorer*', 'https://moz.com/researchtools/ose/links?filter=&source=external&target=page&group=0&page=1&sort=page_authority&anchor_id=&anchor_type=&anchor_text=&from_site=&site=', 1, 'share'),
+      array('WebAIM: WAVE Accessibility Tool', 'https://wave.webaim.org/report#/', 0, 'universal-access-alt'),
+      array('Toptal: Colorblind Web Page Filter', 'https://www.toptal.com/designers/colorfilter?process_type=deutan&orig_uri=', 0, 'universal-access-alt'),
+      array('Tenon: Accessibility Test*', 'https://tenon.io/testNow.php?url=', 0, 'universal-access-alt'),
+);
+
+/*
+ * Webpage-checking services that need URL entered at site.
+ *
+ * array({Service-name}, {Service-URL}, {API?}, {dashicon})
+ */
+$checkers_links = array(
+    array('WebPagetest', 'https://www.webpagetest.org/', 1, 'performance'),
+    array('Pingdom: Website Speed Test', 'https://tools.pingdom.com/', 0, 'performance'),
+    array('Sonarwhal: Scanner', 'https://sonarwhal.com/scanner', 0, 'performance'),
+    array('Twitter: Card Validator', 'https://cards-dev.twitter.com/validator', 0, 'share'),
+    array('AChecker: Web Accessibility Checker', 'https://achecker.ca/checker/index.php', 1, 'universal-access-alt'),
+);
+
+/*
+ * Site-checking services that process results with domain in query string.
+ *
+ * array({Service-name}, {Service-URL-prefix}, {domain?}, {dashicon})
+ */
+$checkers_sites = array(
+    array('SimilarWeb', 'https://www.similarweb.com/website/', 0, 'chart-line'),
+    array('Alexa', 'https://www.alexa.com/siteinfo/', 0, 'chart-line'),
+    array('Quantcast', 'https://www.quantcast.com/', 0, 'chart-line'),
+    array('Sucuri', 'https://sitecheck.sucuri.net/', 0, 'lock'),
+    array('SSL Labs', 'https://www.ssllabs.com/ssltest/analyze.html?d=', 0, 'lock'),
+    array('W3Techs', 'https://w3techs.com/sites/info/', 0, 'editor-code'),
+    array('BuiltWith', 'https://builtwith.com/', 0, 'editor-code'),
+);
 
 /* ------------------------------------------------------------------------ *
  * Settings screen
@@ -156,8 +192,9 @@ function checkers_settings_display() {
                 </div>
             </div><!-- #find-posts -->
         </form>
-
-        <p><?php _e( ' Check a webpage for <span class="dashicons-before dashicons-performance">performance</span>, <span class="dashicons-before dashicons-universal-access-alt">accessibility</span>, and <span class="dashicons-before dashicons-share">social shares</span>.', 'checkers' ); ?></p>
+        <p><?php _e( 'Get results from online webpage and website checkers.', 'checkers' ); ?></p>
+        <h2 id="checkers-page"><?php _e('Page checkers', 'checkers' ); ?></h2>
+        <p><?php _e( 'Check a webpage for <span class="dashicons-before dashicons-performance">performance</span>, <span class="dashicons-before dashicons-universal-access-alt">accessibility</span>, and <span class="dashicons-before dashicons-share">social shares</span>.', 'checkers' ); ?></p>
         <form id="checkers-form">
             <?php $checkers_url_val = ( isset( $_POST['found_post_id'] ) ) ? get_permalink( $_POST['found_post_id'] ) : ''; ?>
             <p><label for="url">Enter URL (or <a href="#checkers-url" onclick="findPosts.open( 'action','find_posts' ); return false;" class="hide-if-no-js aria-button-if-js" aria-label="Open search-posts list form" role="button">Search Posts</a>):</label><br>
@@ -165,7 +202,16 @@ function checkers_settings_display() {
             <input type="submit" value="Submit URL" class="button button-primary" />
         </form>
         <figure id="checkers-results" style="margin: 0; max-width: 40rem;">
+        <p><?php _e('Submit an URL to get results from these online webpage checking services:', 'checkers') ?></p>
+        <?php echo checkers_page_services(); ?>
         </figure>
+        <p class="description"><?php _e('* Service limits the number of daily checks.', 'checkers') ?></p>
+        <hr>
+
+        <h2 id="checkers-site"><?php _e('Site checkers', 'checkers' ); ?></h2>
+            <p><?php _e( ' Check your website for <span class="dashicons-before dashicons-chart-line">statistics</span>, <span class="dashicons-before dashicons-lock">security</span>, and <span class="dashicons-before dashicons-editor-code">technologies</span>.', 'checkers' ); ?></p>
+        <?php echo checkers_site_services(); ?>
+
     </div><!-- .wrap -->
     <?php
 }
@@ -179,26 +225,68 @@ function checkers_settings_display() {
  */
 function checkers_load_admin_scripts( $hook ) {
 	global $checkers_options_page; // Hook for this screen.
+    global $checkers_pages, $checkers_links, $checkers_sites;
 
     if ( $checkers_options_page == $hook ) { // Load only on this screen.
         // Used to display and process the search-post modal form.
         wp_enqueue_style('thickbox');
         wp_enqueue_script('thickbox');
         wp_enqueue_script('media');
-        wp_enqueue_script('wp-ajax-response');
+        // wp_enqueue_script('wp-ajax-response');
 
         // Used to process the page checkers form.
         $file_path = plugin_dir_path( __FILE__ ) . 'js/checkers-ajax.js';
     	wp_enqueue_script( 'checkers-ajax', plugin_dir_url( __FILE__ ) . 'js/checkers-ajax.js', array('jquery'), filemtime( $file_path ) );
     	wp_localize_script('checkers-ajax', 'checkers_vars', array(
     			'checkers_nonce' => wp_create_nonce('checkers-nonce'),
-                'checkers_p'     => __('Submit an URL to get results from these online webpage checking services:', 'checkers'),
-                'checkers_p_top' => __('These links open a new browser window with your results from:', 'checkers'),
-                'checkers_p_mid' => __('The above links start processing your results. The services below require entering an URL at their site. Your URL is in your clipboard, ready to paste into their field.', 'checkers'),
-                'checkers_note'  => __('* Service limits the number of daily checks.', 'checkers'),
+                'checkers_p_top' => __('These links open a new browser window which starts processing your results from:', 'checkers'),
+                'checkers_p_mid' => __('These services require you enter an URL at their site. Your URL is now in your clipboard, ready to paste into their field.', 'checkers'),
                 'checkers_else'  => __('Enter a valid URL.', 'checkers'),
+                'checkers_pages' => $checkers_pages,
+                'checkers_links' => $checkers_links,
+                'checkers_sites' => $checkers_sites,
     		)
     	);
     }
 }
 add_action('admin_enqueue_scripts', 'checkers_load_admin_scripts');
+
+/**
+ * Build HTML list of page-checking services.
+ *
+ * @since   0.1.0
+ *
+ * @return string $links HTML ordered list.
+ */
+function checkers_page_services() {
+    global $checkers_pages;
+    $links = '<ol>';
+    foreach ( $checkers_pages as $site ) {
+        $links .= '<li class="dashicons-before dashicons-' . esc_attr( $site[3] ) . '">';
+        $links .= esc_html( $site[0]) . '</li>';
+    }
+    $links .= '</ol>';
+
+    return $links;
+}
+
+/**
+ * Build HTML list of site-checking service links.
+ *
+ * @since   0.1.0
+ *
+ * @return string $links HTML ordered list.
+ */
+function checkers_site_services() {
+    global $checkers_sites;
+    $host = parse_url( get_site_url(), PHP_URL_HOST);
+    $links = '<ol>';
+    foreach ( $checkers_sites as $site ) {
+        $links .= '<li class="dashicons-before dashicons-' . esc_attr( $site[3] ) . '">';
+        $links .= '<a href="' . esc_url( $site[1] . $host ) . '" target="_blank">';
+        $links .= esc_html( $site[0]) . '</a></li>';
+    }
+    $links .= '</ol>';
+
+    return $links;
+}
--- js/checkers-ajax.js
+++ js/checkers-ajax.js
@@ -1,83 +1,44 @@
 var checks, services, check_url, check_links, i;
 
-/*
- * Online page checkers that can process results with URL in query string.
- *
- * [{Service-name}, {Service-URL-prefix}, {encode?}, {dashicon} ]
- */
-checks = [
-      ['Google: Pagespeed Insights', 'https://developers.google.com/speed/pagespeed/insights/?url=', 1, 'performance'],
-      ['Google: Mobile-Friendly Test', 'https://search.google.com/test/mobile-friendly?url=', 1, 'performance'],
-      ['W3C: Markup Validation', 'https://validator.w3.org/checklink?hide_type=all&depth=&check=Check&uri=', 1, 'performance'],
-      ['Twitter: Search (post URL)', 'https://twitter.com/search?src=typd&q=', 1, 'share'],
-      ['Facebook: Link Preview', 'https://developers.facebook.com/tools/debug/sharing/?q=', 1, 'share'],
-      ['Facebook: Shares (data)', 'https://graph.facebook.com/?id=', 0, 'share'],
-      ['LinkedIn: Shares (data)', 'https://www.linkedin.com/countserv/count/share?url=', 0, 'share'],
-      ['Moz: Open Site Explorer*', 'https://moz.com/researchtools/ose/links?filter=&source=external&target=page&group=0&page=1&sort=page_authority&anchor_id=&anchor_type=&anchor_text=&from_site=&site=', 1, 'share'],
-      ['WebAIM: WAVE Accessibility Tool', 'https://wave.webaim.org/report#/', 0, 'universal-access-alt'],
-      ['Toptal: Colorblind Web Page Filter', 'https://www.toptal.com/designers/colorfilter?process_type=deutan&orig_uri=', 0, 'universal-access-alt'],
-      ['Tenon: Accessibility Test*', 'https://tenon.io/testNow.php?url=', 0, 'universal-access-alt'],
-];
-
-/*
- * Online page checking services that need URL entered at site,
- *
- * [{Service-name}, {Service-URL}, {API?}, {dashicon} ]
- */
-services = [
-    ['WebPagetest', 'https://www.webpagetest.org/', 1, 'performance'],
-    ['Pingdom: Website Speed Test', 'https://tools.pingdom.com/', 0, 'performance'],
-    ['Sonarwhal: Scanner', 'https://sonarwhal.com/scanner', 0, 'performance'],
-    ['Twitter: Card Validator', 'https://cards-dev.twitter.com/validator', 0, 'share'],
-    ['AChecker: Web Accessibility Checker', 'https://achecker.ca/checker/index.php', 1, 'universal-access-alt'],
-];
-
 window.onload = function() {
-    /* Initial page information, with list of page checkers. */
-    check_links = '';
-    check_links += '<p>' + checkers_vars.checkers_p + '</p>';
-    check_links += '<ol>';
-    for ( i = 0; i < checks.length; i++ ) { // Array of checker data.
-        check_links += '<li class="dashicons-before dashicons-' + checks[i][3] + '">' + checks[i][0] + '</li>';
-    }
-    check_links += '</ol>';
-    check_links += '<p class="description">' + checkers_vars.checkers_note + '</p>';
-
-    document.getElementById('checkers-results').innerHTML = check_links;
+    alert( checkers_vars.checkers_links[0][1] );
 
     /* Indicate a change to user (needed when submitting a 2nd URL). */
-    document.getElementById('checkers-url').addEventListener('focus', function(){
-        document.getElementById('checkers-results').style.backgroundColor = '#fffff3';
+    document.getElementById( 'checkers-url' ).addEventListener( 'focus', function(){
+        document.getElementById( 'checkers-results' ).style.backgroundColor = '#fffff3';
     });
 
     /* Process form submission */
-    document.getElementById('checkers-form').addEventListener('submit', function( event ){
+    document.getElementById( 'checkers-form' ).addEventListener( 'submit', function( event ){
         event.preventDefault();
         check_links = '';
-        let check_url_input = document.getElementById('checkers-url'); // Get form input.
+        let check_url_input = document.getElementById( 'checkers-url' ); // Get form input.
 
         if ( check_url_input && check_url_input.value ) { // Input field has value.
             check_url = check_url_input.value;
 
             // Select URL and copy into clipboard.
             check_url_input.select();
-            document.execCommand("Copy");
+            document.execCommand( 'Copy' );
+            check_url_input.blur(); // Remove select.
 
             /* Processed page information, with list of links to page checkers. */
             check_links += '<p>' + checkers_vars.checkers_p_top + '</p>';
             check_links += '<ol>';
-            for ( i = 0; i < checks.length; i++ ) { // Array of checker data.
+            c_pages = checkers_vars.checkers_pages;
+            for ( i = 0; i < c_pages.length; i++ ) { // Array of checker data.
                 // Some checkers needed encoded URL.
-                check_url = ( checks[i][2] ) ? encodeURIComponent( check_url ) : check_url;
+                check_url = ( c_pages[i][2] ) ? encodeURIComponent( check_url ) : check_url;
                 // Build HTML list of links.
-                check_links += '<li class="dashicons-before dashicons-' + checks[i][3] + '"><a href="' + checks[i][1] + check_url + '" target="_blank">' + checks[i][0] + '</a></li>';
+                check_links += '<li class="dashicons-before dashicons-' + c_pages[i][3] + '"><a href="' + c_pages[i][1] + check_url + '" target="_blank">' + c_pages[i][0] + '</a></li>';
             }
-            check_links += '</ol>';
-            check_links += '<p class="description">' + checkers_vars.checkers_note + '</p>';
+            check_links += '</ol><hr>';
             check_links += '<p>' + checkers_vars.checkers_p_mid + '</p>';
             check_links += '<ol>';
-            for ( i = 0; i < services.length; i++ ) { // Array of checker data.
-                check_links += '<li class="dashicons-before dashicons-' + services[i][3] + '"><a href="' + services[i][1] + '" target="_blank">' + services[i][0] + '</a></li>';
+
+            c_links = checkers_vars.checkers_links;
+            for ( i = 0; i < c_links.length; i++ ) { // Array of checker data.
+                check_links += '<li class="dashicons-before dashicons-' + c_links[i][3] + '"><a href="' + c_links[i][1] + '" target="_blank">' + c_links[i][0] + '</a></li>';
             }
             check_links += '</ol>';
 
@@ -86,25 +47,10 @@ window.onload = function() {
         }
 
     // Print list.
-    document.getElementById('checkers-results').innerHTML = check_links;
-    document.getElementById('checkers-results').style.backgroundColor = '#F7F7F7';
+    document.getElementById( 'checkers-results' ).innerHTML = check_links;
+    document.getElementById( 'checkers-results' ).style.backgroundColor = '#F7F7F7';
     });
 };
-
-function checkers_format_url( url_to_check, encode_url = 0, hostname_only = 0) {
-    // Some checkers needed encoded URL.
-    url_formatted = ( encode_url ) ? encodeURIComponent( url_to_check ) : url_to_check;
-    // Remove URL protocal; use hostname-only.
-    url_formatted = ( hostname_only ) ? encodeURIComponent( url_to_check ) : url_to_check;
-
-    return url_formatted;
-}
-
-function checkers_link_url( url_complete, service_name, dashicon) {
-    html = '<li class="dashicons-before dashicons-' + dashicon + '"><a href="' + url_complete + '" target="_blank">' + service_name + '</a></li>';
-
-    return html;
-}
 
 /*
 https://www.rjionline.org/stories/true-or-false-politifact-pols-and-pundits
@@ -133,6 +79,7 @@ https://twitter.com/search?q=https%3A%2F%2Fexample.com%2F2018%2F02%2Fexample-pos
 https://moz.com/researchtools/ose/links?site=https%3A%2F%2Fexample.com%2Fexample-post-slug%2F&filter=&source=external&target=page&group=0&page=1&sort=page_authority&anchor_id=&anchor_type=&anchor_text=&from_site=
 https://validator.w3.org/checklink?hide_type=all&depth=&check=Check&uri=https%3A%2F%2Fexample.com%2Fexample-post-slug%2F&
 
+https://app.buzzsumo.com/research/most-shared?type=articles&result_type=total&num_days=365&general_article&infographic&video&how_to_article&list&what_post&why_post&page=1&q=https:%2F%2Fexample.com%2Fexample-post-slug%2F
 
 Use title, encoded (2):
 https://www.facebook.com/search/str/%22Example+Post+Title%22/keywords_search
@@ -143,9 +90,10 @@ https://www.similarweb.com/website/example.com
 https://w3techs.com/sites/info/example.com
 https://www.quantcast.com/example.com
 https://www.alexa.com/siteinfo/example.com
-https://www.ssllabs.com/ssltest/analyze.html?d=current.org&latest
-https://sitecheck.sucuri.net/results/current.org/2018/02/how-to-make-sure-your-website-works-for-all-users/
-https://app.buzzsumo.com/research/most-shared?type=articles&result_type=total&num_days=365&general_article&infographic&video&how_to_article&list&what_post&why_post&page=1&q=http:%2F%2Fcurrent.org%2F2018%2F02%2Fhow-to-make-sure-your-website-works-for-all-users%2F
+https://www.ssllabs.com/ssltest/analyze.html?d=current.org
+https://sitecheck.sucuri.net/results/current.org
+https://builtwith.com/example.com
+
 
 API site checks:
 https://www.webpagetest.org/runtest.php?k={API-key}&runs=1&web10=1&fvonly=1&f=xml&noopt=1&noimages=1&ignoreSSL=1&url=example.com
